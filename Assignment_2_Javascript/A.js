7a5bd775a482a5b5baf3dc3147054ccd9d5d811f
Yukta-04 <73277684+Yukta-04@users.noreply.github.com>
2023-10-31 09:34:41 +0530

    Update A.js

--- Assignment_2_Javascript/A.js
+++ Assignment_2_Javascript/A.js
@@ -1,1 +1,23 @@
+//A. Take a sentence as an input and reverse every word in that sentence. 
+//a. Example - This is a sunny day > shiT si a ynnus yad. 
+  
+function reverseWords(sentence) {
+  
+  var words = sentence.split(" ");
+
+  
+  var reversedWords = words.map(function(word) {
+    return word.split('').reverse().join('');
+  });
+    
+  
+  
+  var reversedSentence = reversedWords.join(' ');
+
+  return reversedSentence;
+}
+
+var sentence = "This is a sunny day";
+var reversedSentence = reverseWords(sentence);
+console.log(reversedSentence); 
 
